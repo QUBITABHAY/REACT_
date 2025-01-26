bc70afab411b5af33aeda35ab7555caaf2949dfb
QUBITABHAY <codingcontestabhay@gmail.com>
2025-01-26 23:55:13 +0530

    coustom

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,34 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Coustom App!</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: "a", 
+//   props: {
+//       href: "https://google.com",
+//       target: "_blank"
+//   },
+//   children: "Click me to visit google"
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Click to visit google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: "https://google.com", target: "_blank"},
+  'Click me to visit google'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
 )
