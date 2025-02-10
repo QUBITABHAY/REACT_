70a72157bc9aaeea39a034dcd3abb73fb4dccbdd
QUBITABHAY <codingcontestabhay@gmail.com>
2025-02-10 23:12:01 +0530

    fixed

--- 07reactrouter/src/components/Github/Github.jsx
+++ 07reactrouter/src/components/Github/Github.jsx
@@ -16,6 +16,8 @@ function Github() {
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
     <img src={data.avatar_url} alt="Git picture" width={300} />
+    {/* Git Data */}
+    <h4>Github Account Created on {data.created_at}</h4>
     </div>
   )
 }
