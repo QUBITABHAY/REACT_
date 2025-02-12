8b2025ebbe9f0f8f6be794e81022cf4cab103823
QUBITABHAY <codingcontestabhay@gmail.com>
2025-02-12 23:30:09 +0530

    context api

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import Login from './component/Login'
+import Profile from './component/Profile'
 import UserContextProvider from './context/UserContextProvider'
 
 function App() {
@@ -9,6 +11,14 @@ function App() {
         <header className="App-header">
           <h1 className="text-4xl font-bold">Mini Context</h1>
         </header>
+        <div className="flex justify-center mt-10">
+          <div className="w-1/3">
+            <Login />
+          </div>
+          <div className="w-1/3">
+            <Profile />
+          </div>
+        </div>
       </div>
     </UserContextProvider>
   )
